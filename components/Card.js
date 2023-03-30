79f5c55f80380d787b2d2a8852809cac659c2c09
Oscar Fuentes <ofuentese@gmail.com>
2023-03-30 16:36:11 -0700

    adding misc to post: date a readingtime

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import styles from '../styles/Card.module.css'
 import { TagsContainer } from './TagsContainer'
+import Image from 'next/image'
+import { dateFormat } from '../lib/dateFormat'
 
 export const Card = ({ post }) => {
   return (
@@ -8,7 +10,7 @@ export const Card = ({ post }) => {
       <h2>{post.title}</h2>
       <p>{post.subtitle}</p>
       <TagsContainer tags={post.tags} />
-      <h4>{`Fecha de publicación: ${post.date}`}</h4>
+      <h4 className={styles.date}><Image src='/calendar.svg' alt='calendar' width={25} height={25} />{dateFormat(post.date)}</h4>
     </Link>
   )
 }
--- components/LayoutPost.js
+++ components/LayoutPost.js
@@ -1,5 +1,7 @@
 import styles from '../styles/Post.module.css'
 import { TagsContainer } from './TagsContainer'
+import Image from 'next/image'
+import { dateFormat } from './../lib/dateFormat'
 
 export const LayoutPost = ({ children, frontmatter }) => {
   return (
@@ -7,6 +9,27 @@ export const LayoutPost = ({ children, frontmatter }) => {
       <h1>{frontmatter.title}</h1>
       <h2>{frontmatter.subtitle}</h2>
       <TagsContainer tags={frontmatter.tags} />
+      <div className={styles.misc}>
+        <p className={styles.text}>
+          <Image
+            src='/calendar.svg'
+            alt='calendar'
+            width={25}
+            height={25}
+          />
+
+          {dateFormat(frontmatter.date)}
+        </p>
+        <p className={styles.text}>
+          <Image
+            src='/clock.svg'
+            alt='clock'
+            width={25}
+            height={25}
+          />
+          {Math.round(frontmatter.readingTime.minutes)} minutos
+        </p>
+      </div>
       {children}
     </div>
   )
